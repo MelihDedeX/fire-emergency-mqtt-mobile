09a974e97078ab27abcaf3a7bf45ac963049d284
MDD <dedemelih.c@gmail.com>
2023-05-26 21:15:05 +0300

    home page and settings page updated

--- app/src/main/java/com/bm/fire_emergency_mqtt_mobile/services/API.kt
+++ app/src/main/java/com/bm/fire_emergency_mqtt_mobile/services/API.kt
@@ -1,7 +1,6 @@
 package com.bm.fire_emergency_mqtt_mobile.services
 
 import com.bm.fire_emergency_mqtt_mobile.models.Entity
-import com.bm.fire_emergency_mqtt_mobile.utilities.preferences.SharedPreferencesToken
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Response
@@ -15,13 +14,12 @@ inline fun httpClientInterceptor(): OkHttpClient {
         override fun intercept(chain: Interceptor.Chain): Response {
             val request = chain.request()
 
-            val token = SharedPreferencesToken.token
+            //val token = SharedPreferencesToken.token
             val requestBuild = request.newBuilder()
-                .addHeader("Authorization", "${token}")
+                //   .addHeader("Authorization", "Bearer ${SharedPreferencesToken.token}")
                 .build()
             return chain.proceed(requestBuild)
         }
-
     })
     val client = httpClient.build()
     return client
@@ -30,7 +28,7 @@ inline fun httpClientInterceptor(): OkHttpClient {
 // S is a service T is a model
 // for example url = https://localhost:44303/api/products or categories or students
 inline fun <reified S, reified T : Entity> api(fixUrl: Boolean = true): S {
-    var baseUrl = "https://2708-88-226-107-206.ngrok-free.app/api/"
+    var baseUrl = "https://c93d-24-133-248-36.ngrok-free.app/api/"
 
     //When we send request to API must put the model name, but some model name might end with 'y' character.
     // we always use plural name in API that's way if it was ended with 'y' character we need to remove 'y' and we need to put 'ies' end of the model name
